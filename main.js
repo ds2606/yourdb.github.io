c9480a9ab77360150ff0cf645798dfa376e9f73f
Daniel Schreck <daniel.s.schreck@gmail.com>
2022-05-26 02:05:10 -0400

    Fill mast with content

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 $(() => {
-    $(window).scroll(function() {
+    function toggleNavDark() {
         console.log('abc');
         $('.navbar')[$(window).scrollTop() > 0 ? 'addClass' : 'removeClass']('scrolled');
         if ($(window).scrollTop() > 0)
             $('.navbar').removeClass('navbar-light').addClass('navbar-dark');
         else
             $('.navbar').removeClass('navbar-dark').addClass('navbar-light');
-    });
+    }
+    $(window).scroll(toggleNavDark);
+    toggleNavDark();
 });
