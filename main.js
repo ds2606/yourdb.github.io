d79b6edf8fc23bcf9753750076af9bb166e05347
Daniel Schreck <daniel.s.schreck@gmail.com>
2022-05-26 00:25:05 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,10 @@
+$(() => {
+    $(window).scroll(function() {
+        console.log('abc');
+        $('.navbar')[$(window).scrollTop() > 0 ? 'addClass' : 'removeClass']('scrolled');
+        if ($(window).scrollTop() > 0)
+            $('.navbar').removeClass('navbar-light').addClass('navbar-dark');
+        else
+            $('.navbar').removeClass('navbar-dark').addClass('navbar-light');
+    });
+});
